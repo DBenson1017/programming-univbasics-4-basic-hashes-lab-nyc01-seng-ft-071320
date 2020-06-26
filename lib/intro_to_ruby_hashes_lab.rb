00265999122bf5ac4ecec376815c56ff2366ed3b
Devin Benson <devinbenson@gmail.com>
2020-06-26 19:14:31 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -11,9 +11,9 @@ end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
-  pioneer = {}
-  pioneer[:name] = "Grace Hopper"
-  pioneer{}
+  pioneer = {
+    :name "Grace Hopper"
+  }
 end
 
 
