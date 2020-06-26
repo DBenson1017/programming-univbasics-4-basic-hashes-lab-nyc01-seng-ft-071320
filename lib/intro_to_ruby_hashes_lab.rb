6836526ecbbb440c92fd367c9f26fec0fd1dd11f
Devin Benson <devinbenson@gmail.com>
2020-06-26 19:11:46 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -12,13 +12,13 @@ end
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
   new_hash = {}
-  new_hash{:name} = "Grace Hopper"
+  new_hash[:name] = "Grace Hopper"
 end
 
 
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
   id_hash = {} 
-  id_hash{:id} = number  
+  id_hash[:id] = number  
   
 end
