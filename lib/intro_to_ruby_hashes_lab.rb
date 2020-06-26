74d1f3d37da7ab1b71045cc8c1ce76a145df28a7
Devin Benson <devinbenson@gmail.com>
2020-06-26 19:04:06 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,16 +1,23 @@
 def new_hash
-  # return an empty hash
+  hash = {}
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  my_hash = {}
+  my_hash{:name} = "Devin"
 end
+
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  new_hash = {}
+  new_hash{:name} = "Grace Hopper"
 end
 
 
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
+  id_hash = {} 
+  id_hash{:id} = number  
+  
 end
